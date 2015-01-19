5094bb74269deee0f6af2615d57ba1ba3fc6a9af
Blarry Wang <blarrywang@gmail.com>
2015-01-19 14:43:47 -0500

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+// interface usableItem{
+	// void useItem();
+// }
+
+
+public class Item{
+	String name;
+	String ld;
+	int powerEnergy;
+	String td;
+	
+	
+	Item(String name, String ld, int powerEnergy, String td){
+		this.name=name;
+		this.powerEnergy=powerEnergy;
+		this.ld=ld;
+		this.td=td;
+	}
+	
+	
+	
+}
