68a7664803c9303c5a5e9fe4472a7a784954506b
Blarry Wang <blarrywang@gmail.com>
2015-01-19 14:42:56 -0500

    Update Map.java

--- Map.java
+++ Map.java
@@ -2,80 +2,89 @@
 import java.util.*;
 public class Map {
 	
-	List<Room> Map = new ArrayList<Room>();
+	private List<Room> roomMap = new ArrayList<Room>();
 	
-	void initMap(){
-		Map.add(new Room(1,"Office1","You are in a dirty office. There is an exits to N.",
-				0,5,0,0,0,0));
-		Map.add(new Room(2,"Office2","You are in an awesome office. There is an exits to S.",
-				5,0,0,0,0,0));
-		Map.add(new Room(3,"Office3","You are in a smelly office. There is an exits to E.",
-				0,0,0,5,0,0));
-		Map.add(new Room(4,"Office4","You are in a lolly office. There is an exits to W.",
-				0,0,5,0,0,0));
-		Map.add(new Room(5,"Boardroom","You are in a rotfy boardroom. There are exits to all directions!",
-				1,2,3,4,0,0));
+	private void initroomMap(){
+		Room r;
+		
+		r = new Room(1,"Office1","You are in a dirty office. There is an exit to the S.",
+				0,5,0,0,0,0,false,0); 
+		r.items.add(new Item("sword","this is a powerful elven sword", 10, "Ow, that's going to be a bad one in the bathroom later."));
+		roomMap.add(r);
+				
+		r = new Room(2,"Office2","You are in an awesome office. There is an exit to the N.",
+				5,0,0,0,0,0,false,0);
+		r.items.add(new Item("knife","this is a very sharp knife", 2, "Ouch! Gotta love the taste of internal bleeding."));
+		roomMap.add(r);
+				
+		r = new Room(3,"Office3","You are in a smelly office. There is an exit to the W.",
+				0,0,0,5,0,0,false,0);
+		r.items.add(new Item("pen","this is the most powerful weapon in the world", 100, "Mmmm... Inky!"));
+		roomMap.add(r);
+				
+		r = new Room(4,"Office4","You are in a lolly office. There is an exit to the E.",
+				0,0,5,0,0,0,false,0);
+		r.items.add(new Item("keyboard","this can be used as a keyboard or a kayboard", 7, "Yum! Crunchy KEY Lime flavour!"));
+		roomMap.add(r);
+				
+		r = new Room(5,"Boardroom","You are in a rotfy boardroom. There are exits in all directions!",
+				1,2,3,4,0,6,false,0);
+		r.items.add(new Item("keyboard","this can be used as a keyboard or a keyboard", 7, "Mmmm... Alphapet Soup flavour!"));
+		r.items.add(new Item("notebook","this is a compliment product of pens", 2, "The ink-slewn paper is mouth-watering."));
+		roomMap.add(r);
+		
+		r = new Room(6, "Break Room", "You are in a pally break room. There is a button stuck to the table!",
+				0,0,0,0,0,7,true,10);
+		r.items.add(new Item("sword","this is a powerful nine sword", 9, "That felt interesting."));
+		//r.items.add(new Item("Sword","this is a powerful elven sword", 10));
+		roomMap.add(r);
+		
+		r = new Room(7, "IT Room", "A dark basement room. There is a red door to the west which you shouldn't open.",
+				0,0,0,8,0,0,false,0);
+		r.items.add(new Item("sword","this is a powerful nine sword", 9, "That felt interesting."));
+		roomMap.add(r);
+		
+		r = new Room(8, "Server Room", "A room with a box with lots of lights and buttons. There is " +
+		"a man who looks like a vampire, his name is Richmond.", 0,9,7,0,0,0, true, 5);
+		r.items.add(new Item("richmond", "this is a man who looks like a vampire", 1, "You disgust me, you cannibal."));
+		roomMap.add(r);
+		
+		r = new Room(9, "Invention Room", "A glass case saying 'Newest Prototype: TeleWatch' is on the table.",8,0,0,0,0,0, false, 0);
+		r.items.add(new Item("timewatch", "I think I can use this to teleport in a new dimension",10, "Great. Now you're never going to get home."));
+		roomMap.add(r);
+		
+		r = new Room(10, "Hollywood", "You are back home, a beautiful place on earth where Wacky Worlds don't exsist. People are staring" + 
+		"at you because you just appered in the middle of the street with no clothes. You grab someone's Statue of Liberty Towel and run to your house.",0,0,0,0,0,0, false,0);
+		roomMap.add(r);
+		
+		r = new Room(11, "Forge Room", "It's so hot here!",0,0,0,0,0,0, false,0);
+		roomMap.add(r);
+		
+		
 	}
+	
+	/*take sword: 
+	- copy all of the item info, and make a new identical item that is added to the inventory
+	- delete item from room	
+	Drop sword:
+	- make a copy, put the copy into the current room
+	- delete the sword item from the inventory
+	*/
+	
 	Map(){
-		initMap();
+		initroomMap();
 	}
 
 
-
+	int mapSize() {
+		return roomMap.size();
+	}
+	
 	Room getRoomByIndex(int index){
-		for(Room room: Map){
+		for(Room room: roomMap){
 			if(room.number==index) return room;
 		}
 		return null; // null means nothing's there
 	}
 }
 
-//Room class
-class Room {
-	int number;
-	int N, E, S, W, U, D;
-	//List<Item> Map = new ArrayList<Item>(); //2 B Finished
-	//Employee owner;
-	String sd, ld;
-	boolean isLit;
-	
-	
-	Room(int n, String sd, String ld, int N, int S,int E,int W,int U,int D){
-		number = n;
-		this.N= N; 
-		this.E= E; 
-		this.S= S; 
-		this.W= W; 
-		this.U= U; 
-		this.D= D; 
-		this.ld= ld; 
-		this.sd= sd; 
-		//this.isLit=isLit;
-		//init items here
-	}
-}
-/*
-class Corridor {
-	 Corridor (int n, String sd, String ld, int e1, int e2){
-		number = n; 
-		this.ld= ld; 
-		this.sd= sd; 
-		this.e1=e1;
-		this.e2=e2;
-		//init items here
-	}
-	 
-	int number;
-	int e1, e2;
-	int[] room= new int[20]; //connected to 20 rooms
-	
-	String sd, ld;
-	
-	//List<Item> Map = new ArrayList<Item>(); //2 B Finished
-	
-	
-	
-	
-	
-}
-*/
